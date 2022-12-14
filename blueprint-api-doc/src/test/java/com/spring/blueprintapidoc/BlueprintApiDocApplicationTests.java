c333b059078b98b836a9926b017430d96623266e
Serhat Selim <serhatselim@hotmail.com.tr>
2022-12-14 17:30:07 +0300

    api swagger

---
+++ blueprint-api-doc/src/test/java/com/spring/blueprintapidoc/BlueprintApiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blueprintapidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprintApiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
