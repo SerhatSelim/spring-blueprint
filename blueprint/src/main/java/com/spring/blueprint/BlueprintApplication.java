c1a39f739a36f283c44a75f56bfb65d99a773550
Serhat Selim <serhatselim@hotmail.com.tr>
2022-12-14 15:05:52 +0300

    terminal

--- blueprint/src/main/java/com/spring/blueprint/BlueprintApplication.java
+++ blueprint/src/main/java/com/spring/blueprint/BlueprintApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlueprintApplication {
 
 	public static void main(String[] args) {
+		System.out.print("blueprint"); 
+		
 		SpringApplication.run(BlueprintApplication.class, args);
+
+		
 	}
 
 }
